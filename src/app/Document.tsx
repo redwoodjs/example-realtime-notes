90292228911af2c28fb4cf43b32f24aaa4e791c8
justinvdm <justinvderm@gmail.com>
2025-04-28 11:44:56 +0200

    Upgrade to .76

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -8,11 +8,12 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-standard</title>
+      <link rel="modulepreload" href="/src/client.tsx" />
       <TurnstileScript />
-      <script type="module" src="/src/client.tsx"></script>
     </head>
     <body>
       <div id="root">{children}</div>
+      <script>import("/src/client.tsx")</script>
     </body>
   </html>
 );
